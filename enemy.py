50f1ddb65a8ad6f73cd71758906bbb6736f7ae49
mwalmer <75793394+mwalmer@users.noreply.github.com>
2021-06-05 23:31:32 -0400

    added enemy and tower classes, along with their sprites

---
+++ enemy.py
@@ -0,0 +1,19 @@
+import pygame.transform
+
+
+class Enemy:
+    def __init__(self, name, health, speed, rect, sprite):
+        self.name = name
+        self.health = health
+        self.speed = speed
+        self.rect = rect
+        self.x = rect.x
+        self.y = rect.y
+        self._sprite = sprite
+        self.sprite = sprite
+
+    def face(self, deg):
+        self.sprite = pygame.transform.rotate(self._sprite, deg)
+
+    def cords(self):
+        return self.x, self.y
--- main.py
+++ main.py
@@ -1,54 +1,98 @@
+from enemy import Enemy
+from tower import Tower
 import pygame
 import os
 
-WIDTH, HEIGHT = 640, 640
+WIDTH, HEIGHT = 800, 640
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Python Defense")
 
-WHITE = (255, 255, 255)
-
 FPS = 60
 
+# Directions
+UP, LEFT, DOWN, RIGHT = 0, 90, 180, 270
+
+# Sizes
+TILE_WIDTH, TILE_HEIGHT = 32, 32
+TOWER_WIDTH, TOWER_HEIGHT = 28, 28
+ENEMY_WIDTH, ENEMY_HEIGHT = 16, 16
+
+# Load image
 GRASS_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'grass_tile.png'))
 DIRT_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'dirt_tile.png'))
+MENU_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'menu_tile.png'))
+TOWER_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower.png'))
+ENEMY_SPRITE = pygame.image.load(os.path.join('assets', 'enemies', 'enemy.png'))
+
+# 0 = grass
+# 1 = dirt
+MAP = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2]]
 
 
-MAP = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+def update(enemies, towers):
+    for enemy in enemies:
+        enemy.y += .5 * enemy.speed
 
 
-def draw_window():
-    WIN.fill(WHITE)
+def draw_window(enemies, towers):
+    # draws map
     for x, row in enumerate(MAP):
         tile = GRASS_TILE
         for y, cord in enumerate(row):
+            # draws grass and path
+            # needs to be drawn before enemies or towers
             if cord == 0:
                 tile = GRASS_TILE
             elif cord == 1:
                 tile = DIRT_TILE
+            elif cord == 2:
+                tile = MENU_TILE
             WIN.blit(tile, (y * 32, x * 32))
+
+    for enemy in enemies:
+        WIN.blit(enemy.sprite, enemy.cords())
+    for tower in towers:
+        WIN.blit(tower.sprite, tower.cords())
+
     pygame.display.update()
 
 
 def main():
+    # TODO: enemy path finding
+    enemies = []
+    for count in range(0, 10):
+        enemy_rect = pygame.Rect(8, 8, ENEMY_WIDTH, ENEMY_HEIGHT)
+        enemy = Enemy(f'enemy_{count}', 100, 1, enemy_rect, ENEMY_SPRITE)
+        enemy.face(DOWN)
+        enemy.y = count * -32
+        enemies.append(enemy)
+
+    towers = []
+    #
+    # for x in range(0, 2):
+    #     tower_rect = pygame.Rect(2, 2, TOWER_WIDTH, TOWER_HEIGHT)
+    #     tower = Tower(f'tower_{x}', 10, 3, tower_rect, TOWER_SPRITE)
+    #     towers.append(tower)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -56,7 +100,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+
+        # update logic
+        update(enemies, towers)
+
+        # refresh/redraw display
+        draw_window(enemies, towers)
+
     pygame.quit()
 
 
