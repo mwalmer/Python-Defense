58e3624186d2d0b5c66790ad7b82ec4ce9e6fcc1
mwalmer <75793394+mwalmer@users.noreply.github.com>
2021-06-12 04:16:37 -0400

    adjusted window scaling, path finding, and turret placement

--- main.py
+++ main.py
@@ -4,50 +4,74 @@ from player import Player
 import pygame
 import os
 
+
+def scale(num):
+    return int(num / 32 * (32 * RATIO))
+
+
+RATIO = 1.5
 pygame.display.init()
-infoObject = pygame.display.Info()
-WIDTH = infoObject.current_w
-HEIGHT = infoObject.current_h
-# Sets w/h to max of whatever screen we're using
-# WIDTH, HEIGHT = 800, 640
+display_info = pygame.display.Info()
+width = display_info.current_w
+height = display_info.current_h
 
-# Scaling pixels to screen ratio
-if WIDTH > HEIGHT:
-    scale = HEIGHT - (HEIGHT % 25) - 50  # -50 is for top window bar
+# Scaling pixels to fixed ratio
+# adjust this to change window size
+if height > width:
+    RATIO = 1.5 if height <= 1920 else 2
 else:
-    scale = WIDTH - (WIDTH % 25) - 50
+    RATIO = 1.5 if width <= 1920 else 2
 
-WIN = pygame.display.set_mode((scale, scale))
+# Default tile size
+TILE_SIZE = scale(32)
+TILE_XY = (TILE_SIZE, TILE_SIZE)
+
+NUM_TILES_X, NUM_TILES_Y = 25, 20
+
+WIDTH = TILE_SIZE * NUM_TILES_X
+HEIGHT = TILE_SIZE * NUM_TILES_Y
+
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Python Defense")
-FPS = 60
 
-# changing scale to be for scaling tiles/towers/etc.
-scale = scale / 25
+FPS = 60
 
 # Directions
 UP, LEFT, DOWN, RIGHT = 0, 90, 180, 270
 
 # Sizes
-TILE_WIDTH, TILE_HEIGHT = scale, scale
-TOWER_WIDTH, TOWER_HEIGHT = 32 / 32 * scale, 32 / 32 * scale
-ENEMY_WIDTH, ENEMY_HEIGHT = 32 / 32 * scale, 32 / 32 * scale
-FIRE_PROJECTILE_WIDTH, FIRE_PROJECTILE_HEIGHT = 16 / 32 * scale, 16 / 32 * scale
+TOWER_SIZE = TILE_SIZE
+ENEMY_SIZE = TILE_SIZE
+FIRE_PROJECTILE_SIZE = scale(16)
 
 # Load image
 GRASS_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'grass_tile.png'))
 DIRT_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'dirt_tile.png'))
 MENU_TILE = pygame.image.load(os.path.join('assets', 'tiles', 'menu_tile.png'))
-TOWER_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower1.png'))
-ENEMY_SPRITE = pygame.image.load(os.path.join('assets', 'enemies', 'enemy1.png'))
+TOWER1_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower1.png'))
+TOWER2_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower2.png'))
+TOWER3_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower3.png'))
+TOWER4_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower4.png'))
+TOWER5_SPRITE = pygame.image.load(os.path.join('assets', 'towers', 'tower5.png'))
+ENEMY1_SPRITE = pygame.image.load(os.path.join('assets', 'enemies', 'enemy1.png'))
+ENEMY2_SPRITE = pygame.image.load(os.path.join('assets', 'enemies', 'enemy2.png'))
+ENEMY3_SPRITE = pygame.image.load(os.path.join('assets', 'enemies', 'enemy3.png'))
 FIRE_PROJECTILE_SPRITE = pygame.image.load(os.path.join('assets', 'projectiles', 'fireball.png'))
 
 # Scale images
-GRASS_TILE = pygame.transform.scale(GRASS_TILE, (int(TILE_WIDTH), int(TILE_HEIGHT)))
-DIRT_TILE = pygame.transform.scale(DIRT_TILE, (int(TILE_WIDTH), int(TILE_HEIGHT)))
-MENU_TILE = pygame.transform.scale(MENU_TILE, (int(TILE_WIDTH), int(TILE_HEIGHT)))
-TOWER_SPRITE = pygame.transform.scale(TOWER_SPRITE, (int(TOWER_WIDTH), int(TOWER_HEIGHT)))
-ENEMY_SPRITE = pygame.transform.scale(ENEMY_SPRITE, (int(ENEMY_WIDTH), int(ENEMY_HEIGHT)))
-FIRE_PROJECTILE_SPRITE = pygame.transform.scale(FIRE_PROJECTILE_SPRITE, (int(ENEMY_WIDTH), int(ENEMY_HEIGHT)))
+GRASS_TILE = pygame.transform.scale(GRASS_TILE, TILE_XY)
+DIRT_TILE = pygame.transform.scale(DIRT_TILE, TILE_XY)
+MENU_TILE = pygame.transform.scale(MENU_TILE, TILE_XY)
+TOWER1_SPRITE = pygame.transform.scale(TOWER1_SPRITE, (TOWER_SIZE, TOWER_SIZE))
+TOWER2_SPRITE = pygame.transform.scale(TOWER2_SPRITE, (TOWER_SIZE, TOWER_SIZE))
+TOWER3_SPRITE = pygame.transform.scale(TOWER3_SPRITE, (TOWER_SIZE, TOWER_SIZE))
+TOWER4_SPRITE = pygame.transform.scale(TOWER4_SPRITE, (TOWER_SIZE, TOWER_SIZE))
+TOWER5_SPRITE = pygame.transform.scale(TOWER5_SPRITE, (TOWER_SIZE, TOWER_SIZE))
+ENEMY1_SPRITE = pygame.transform.scale(ENEMY1_SPRITE, (ENEMY_SIZE, ENEMY_SIZE))
+ENEMY2_SPRITE = pygame.transform.scale(ENEMY2_SPRITE, (ENEMY_SIZE, ENEMY_SIZE))
+ENEMY3_SPRITE = pygame.transform.scale(ENEMY3_SPRITE, (ENEMY_SIZE, ENEMY_SIZE))
+FIRE_PROJECTILE_SPRITE = pygame.transform.scale(FIRE_PROJECTILE_SPRITE, (FIRE_PROJECTILE_SIZE, FIRE_PROJECTILE_SIZE))
 
 
 # 0 = grass
@@ -73,20 +97,12 @@ MAP = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 
        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
-       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2]]
-
-
-# MAP IS 25 ACROSS AND 25 DOWN, 5 last columns are for menu
-
+# MAP IS 25 ACROSS AND 20 DOWN, 5 last columns are for menu
 
 
 def update(enemies, towers):
-    pixel_per_frame = 1
+    pixel_per_frame = scale(1)
     for enemy in enemies:
         enemy_pathfinding(enemy)
         enemy.y += pixel_per_frame * enemy.speed * enemy.y_weight
@@ -111,7 +127,7 @@ def draw_window(enemies, towers):
             elif cord == 3:
                 tile = GRASS_TILE
 
-            WIN.blit(tile, (y * TILE_HEIGHT, x * TILE_WIDTH))
+            WIN.blit(tile, (y * TILE_SIZE, x * TILE_SIZE))
     # Scale sprites
     for enemy in enemies:
         WIN.blit(enemy.sprite, enemy.cords())
@@ -126,41 +142,44 @@ def to_start():
         if i != 1:
             temp_count += 1
         else:
-            return temp_count * 32 + 8
+            return scale(temp_count * 32)
 
 
 def enemy_pathfinding(enemy):
-    # TODO -- fix this up more, right now just turns the enemy right
-    if enemy.x == 104 and enemy.y < 136:
+    # TODO: make dynamic pathfinding
+    if enemy.x == scale(96) and enemy.y < scale(128):
         enemy.x_weight, enemy.y_weight = 0, 1
-    elif enemy.x < 424 and enemy.y == 136:
+    elif enemy.x < scale(416) and scale(128) <= enemy.y < scale(350):
         enemy.face(RIGHT)
         enemy.x_weight, enemy.y_weight = 1, 0
-    elif enemy.x == 424 and enemy.y < 360:
+        enemy.y = scale(128) if enemy.y > scale(128) else enemy.y
+    elif enemy.x >= scale(416) and enemy.y < scale(352):
         enemy.face(DOWN)
         enemy.x_weight, enemy.y_weight = 0, 1
-    elif enemy.x >= 234 and enemy.y == 360:
+        enemy.x = scale(416) if enemy.x > scale(416) else enemy.x
+    elif enemy.x > scale(224) and enemy.y >= scale(352):
         enemy.face(LEFT)
         enemy.x_weight, enemy.y_weight = -1, 0
+        enemy.y = scale(352) if enemy.y >= scale(352) else enemy.y
     else:
         enemy.face(DOWN)
         enemy.x_weight, enemy.y_weight = 0, 1
-    # if ((enemy.y + 32) // 32) < 20 and MAP[int((enemy.y + 32) // 32)][int(enemy.x // 32)] == 0 and enemy.x > 0 and enemy.y > 0:
-    #     enemy.face(RIGHT)
+        enemy.x = scale(224) if enemy.x < scale(224) else enemy.x
 
 
 def spawn(enemies, Count, Speed):
     for count in range(0, Count):
-        enemy_rect = pygame.Rect(0, 0, ENEMY_WIDTH, ENEMY_HEIGHT)
-        enemy = Enemy(f'enemy_{count}', 100, Speed, enemy_rect, ENEMY_SPRITE)
+        enemy_rect = pygame.Rect(0, 0, ENEMY_SIZE, ENEMY_SIZE)
+        enemy = Enemy(f'enemy_{count}', 100, Speed, enemy_rect, ENEMY3_SPRITE)
         enemy.face(DOWN)
-        enemy.y = count * -32
+        enemy.y = count * scale(-32)
         enemy.x = to_start()
         enemies.append(enemy)
 
+
 def level(enemies, Lv):
     if Lv == 1:
-        spawn(enemies, 5, 1)
+        spawn(enemies, 10, 5)
 
     if Lv == 2:
         spawn(enemies, 5, 1)
@@ -180,7 +199,7 @@ def main():
     #
     # for x in range(0, 2):
     #     tower_rect = pygame.Rect(2, 2, TOWER_WIDTH, TOWER_HEIGHT)
-    #     tower = Tower(f'tower_{x}', 10, 3, tower_rect, TOWER_SPRITE)
+    #     tower = Tower(f'tower_{x}', 10, 3, tower_rect, TOWER1_SPRITE)
     #     towers.append(tower)
 
     clock = pygame.time.Clock()
@@ -191,14 +210,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                if MAP[mouse_y // 32][mouse_x // 32] == 0:
-                    MAP[mouse_y // 32][mouse_x // 32] = 3
-                    tower_rect = pygame.Rect((mouse_x // 32) * 32, (mouse_y // 32) * 32, TOWER_WIDTH, TOWER_HEIGHT)
-                    fireball_rect = pygame.Rect((mouse_x // 32) * 32, (mouse_y // 32) * 32, FIRE_PROJECTILE_WIDTH,
-                                                FIRE_PROJECTILE_HEIGHT)
-                    towers.append(Tower(f'tower_{tower_count}', 10, 3, tower_rect, TOWER_SPRITE, "Fireball",
+                #  TODO: reformat
+                if MAP[mouse_y // scale(32)][mouse_x // scale(32)] == 0:
+                    MAP[mouse_y // scale(32)][mouse_x // scale(32)] = 3
+                    tower_rect = pygame.Rect((mouse_x // scale(32)) * scale(32), (mouse_y // scale(32)) * scale(32),
+                                             TOWER_SIZE, TOWER_SIZE)
+                    fireball_rect = pygame.Rect((mouse_x // scale(32)) * scale(32), (mouse_y // scale(32)) * scale(32),
+                                                FIRE_PROJECTILE_SIZE, FIRE_PROJECTILE_SIZE)
+                    towers.append(Tower(f'tower_{tower_count}', 10, 3, tower_rect, TOWER1_SPRITE, "Fireball",
                                         fireball_rect, FIRE_PROJECTILE_SPRITE))
                     tower_count += 1
 
